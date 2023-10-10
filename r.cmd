995d861a0da73dac279f00f2fc4b23034d4e680e
Alex Fedosov <DMAlexx@gmail.com>
2023-10-10 18:10:34 -0400

    Add release preparation batch file

---
+++ r.cmd
@@ -0,0 +1,11 @@
+rem prepare release
+rem.
+rem git tag
+rem git tag -a v1.2.15 -m "Release version 1.2.15"
+rem git push origin v1.2.15
+
+
+rem run release
+rem.
+rem call C:\Info\Projects\Docs\FoundAItion\Source\FaiNlp\Scripts\activate.bat
+rem python src\FaiNlp\FaiNlp.py
